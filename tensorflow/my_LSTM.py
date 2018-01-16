24c597d4b6b27cd3bce5ce78383c112b52486ea4
mrthlinh <mrthlinh@gmail.com>
2018-01-16 00:03:44 -0600

    update

--- tensorflow/my_LSTM.py
+++ tensorflow/my_LSTM.py
@@ -68,27 +68,26 @@ training_iters = 50 * len(training_data)
 # # training_iters = 50000
 display_step = 1000
 n_input = 3
-
+max_seq = 20
 # number of units in RNN cell
 n_hidden = 512
 
 # tf Graph input
-x = tf.placeholder("float", [None, n_input, 1])
+x = tf.placeholder("float", [None, max_seq, 1])
 y = tf.placeholder("float", [None, vocab_size])
 
 # RNN output node weights and biases
 weights = tf.Variable(tf.random_normal([n_hidden, vocab_size]),name='w')
 
 biases = tf.Variable(tf.random_normal([vocab_size]),name = 'b')
-
 def RNN(x, weights, biases):
 
     # reshape to [1, n_input]
-    x = tf.reshape(x, [-1, n_input])
+    x = tf.reshape(x, [-1, max_seq])
 
     # Generate a n_input-element sequence of inputs
     # (eg. [had] [a] [general] -> [20] [6] [33])
-    x = tf.split(x,n_input,1)
+    x = tf.split(x,max_seq,1)
 
     # 2-layer LSTM, each layer has n_hidden units.
     # Average Accuracy= 95.20% at 50k iter
@@ -104,8 +103,32 @@ def RNN(x, weights, biases):
 
     # there are n_input outputs but
     # we only want the last output
-    result = tf.matmul(outputs[-1], weights) + biases
-    return result
+    return tf.matmul(outputs[-1], weights) + biases
+# def RNN(x, weights, biases):
+#
+#     # reshape to [1, n_input]
+#     x = tf.reshape(x, [-1, n_input])
+#
+#     # Generate a n_input-element sequence of inputs
+#     # (eg. [had] [a] [general] -> [20] [6] [33])
+#     x = tf.split(x,n_input,1)
+#
+#     # 2-layer LSTM, each layer has n_hidden units.
+#     # Average Accuracy= 95.20% at 50k iter
+#     rnn_cell = rnn.MultiRNNCell([rnn.BasicLSTMCell(n_hidden),rnn.BasicLSTMCell(n_hidden)])
+#
+#     # 1-layer LSTM with n_hidden units but with lower accuracy.
+#     # Average Accuracy= 90.60% 50k iter
+#     # Uncomment line below to test but comment out the 2-layer rnn.MultiRNNCell above
+#     # rnn_cell = rnn.BasicLSTMCell(n_hidden)
+#
+#     # generate prediction
+#     outputs, states = rnn.static_rnn(rnn_cell, x, dtype=tf.float32)
+#
+#     # there are n_input outputs but
+#     # we only want the last output
+#     result = tf.matmul(outputs[-1], weights) + biases
+#     return result
 
 pred = RNN(x, weights, biases)
 
@@ -130,160 +153,28 @@ with tf.Session() as session:
     # saver.restore(session, './model/LSTM_model_n=3.ckpt')
     saver.restore(session, tf.train.latest_checkpoint('./model/'))
 
-    # session.run(tf.global_variables_initializer())
-    #
-    # print("Weight: ", tf.shape(weights.eval()) )
-    # print("Bias: ", tf.shape(biases.eval()))
-    # W = weights.eval()
-    # b = biases.eval()
-    step = 0
-    offset = random.randint(0,n_input+1)
-    end_offset = n_input + 1
-    acc_total = 0
-    loss_total = 0
 
-    # writer.add_graph(session.graph)
-    # offset = 0
-    # while step < 20:
-    # while step < training_iters:
-    #     # Generate a minibatch. Add some randomness on selection process.
-    #     if offset > (len(training_data)-end_offset):
-    #         offset = 0
-    #         # offset = random.randint(0, n_input+1)
-    #
-    #     symbols_in_keys = [ [dictionary[ str(training_data[i])]] for i in range(offset, offset+n_input) ]
-    #     symbols_in_keys = np.reshape(np.array(symbols_in_keys), [-1, n_input, 1])
-    #     # for i in range(0,n_input):
-    #     #     print(reverse_dictionary[symbols_in_keys[0,i,0]])
-    #     symbols_out_onehot = np.zeros([vocab_size], dtype=float)
-    #     symbols_out_onehot[dictionary[str(training_data[offset+n_input])]] = 1.0
-    #     symbols_out_onehot = np.reshape(symbols_out_onehot,[1,-1])
-    #     # print(reverse_dictionary[int(tf.argmax(symbols_out_onehot, 1).eval())])
-    #     # print(reverse_dictionary[symbols_out_onehot[0, 1]])
-    #     acc, loss, onehot_pred = session.run([accuracy, cost, pred],feed_dict={x: symbols_in_keys, y: symbols_out_onehot})
-    #     loss_total += loss
-    #     acc_total += acc
-    #
-    #     # symbols_in = [training_data[i] for i in range(offset, offset + n_input)]
-    #     # symbols_out = training_data[offset + n_input]
-    #     # symbols_out_pred = reverse_dictionary[int(tf.argmax(onehot_pred, 1).eval())]
-    #     # print("%s - [%s] vs [%s] - [%s]" % (symbols_in, symbols_out, symbols_out_pred,acc))
-    #
-    #     if (step+1) % display_step == 0:
-    #         print("Iter= " + str(step+1) + ", Average Loss= " + \
-    #               "{:.6f}".format(loss_total/display_step) + ", Average Accuracy= " + \
-    #               "{:.2f}%".format(100*acc_total/display_step))
-    #         acc_total = 0
-    #         loss_total = 0
-    #         symbols_in = [training_data[i] for i in range(offset, offset + n_input)]
-    #         symbols_out = training_data[offset + n_input]
-    #         symbols_out_pred = reverse_dictionary[int(tf.argmax(onehot_pred, 1).eval())]
-    #         print("%s - [%s] vs [%s]" % (symbols_in,symbols_out,symbols_out_pred))
-    #
-    #     step += 1
-    #     offset += (n_input+1)
-
-    # Examine
-    # step = 0
-    # offset = 0
-    # end_offset = n_input + 1
-    # while step < 30:
-    #     # Generate a minibatch. Add some randomness on selection process.
-    #     if offset > (len(training_data) - end_offset):
-    #         offset = 0
-    #         # offset = random.randint(0, n_input+1)
-    #
-    #     symbols_in_keys = [[dictionary[str(training_data[i])]] for i in range(offset, offset + n_input)]
-    #     symbols_in_keys = np.reshape(np.array(symbols_in_keys), [-1, n_input, 1])
-    #     # for i in range(0,n_input):
-    #     #     print(reverse_dictionary[symbols_in_keys[0,i,0]])
-    #     symbols_out_onehot = np.zeros([vocab_size], dtype=float)
-    #     symbols_out_onehot[dictionary[str(training_data[offset + n_input])]] = 1.0
-    #     symbols_out_onehot = np.reshape(symbols_out_onehot, [1, -1])
-    #     # print(reverse_dictionary[int(tf.argmax(symbols_out_onehot, 1).eval())])
-    #     # print(reverse_dictionary[symbols_out_onehot[0, 1]])
-    #     acc, loss, onehot_pred = session.run([accuracy, cost, pred],
-    #                                          feed_dict={x: symbols_in_keys, y: symbols_out_onehot})
-    #     loss_total += loss
-    #     acc_total += acc
-    #
-    #     symbols_in = [training_data[i] for i in range(offset, offset + n_input)]
-    #     symbols_out = training_data[offset + n_input]
-    #     symbols_out_pred = reverse_dictionary[int(tf.argmax(onehot_pred, 1).eval())]
-    #     print("%s - [%s] vs [%s] - [%s]" % (symbols_in, symbols_out, symbols_out_pred, acc))
-    #
-    #     # if (step+1) % display_step == 0:
-    #     #     print("Iter= " + str(step+1) + ", Average Loss= " + \
-    #     #           "{:.6f}".format(loss_total/display_step) + ", Average Accuracy= " + \
-    #     #           "{:.2f}%".format(100*acc_total/display_step))
-    #     #     acc_total = 0
-    #     #     loss_total = 0
-    #     #     symbols_in = [training_data[i] for i in range(offset, offset + n_input)]
-    #     #     symbols_out = training_data[offset + n_input]
-    #     #     symbols_out_pred = reverse_dictionary[int(tf.argmax(onehot_pred, 1).eval())]
-    #     #     print("%s - [%s] vs [%s]" % (symbols_in,symbols_out,symbols_out_pred))
-    #
-    #     step += 1
-    #     offset += (n_input + 1)
-
-    # print("Weight: ", weights.eval())
-    # print("Bias: ", biases.eval())
-
-    step = 0
-    offset = random.randint(0,n_input+1)
-    end_offset = n_input + 1
-    acc_total = 0
-    loss_total = 0
-
-    # offset = 0
-    while step < training_iters:
-        # Generate a minibatch. Add some randomness on selection process.
-        if offset > (len(training_data)-end_offset):
-            offset = random.randint(0, n_input+1)
-
-        symbols_in_keys = [ [dictionary[ str(training_data[i])]] for i in range(offset, offset+n_input) ]
-        symbols_in_keys = np.reshape(np.array(symbols_in_keys), [-1, n_input, 1])
-
-        symbols_out_onehot = np.zeros([vocab_size], dtype=float)
-        symbols_out_onehot[dictionary[str(training_data[offset+n_input])]] = 1.0
-        symbols_out_onehot = np.reshape(symbols_out_onehot,[1,-1])
-
-        acc, onehot_pred = session.run([accuracy, pred], \
-                                                feed_dict={x: symbols_in_keys, y: symbols_out_onehot})
-        # loss_total += loss
-        acc_total += acc
-        if (step+1) % display_step == 0:
-            print("Iter= " + str(step+1) + ", Average Accuracy= " + \
-                  "{:.2f}%".format(100*acc_total/display_step))
-            acc_total = 0
-
-            symbols_in = [training_data[i] for i in range(offset, offset + n_input)]
-            symbols_out = training_data[offset + n_input]
-            symbols_out_pred = reverse_dictionary[int(tf.argmax(onehot_pred, 1).eval())]
-            print("%s - [%s] vs [%s]" % (symbols_in,symbols_out,symbols_out_pred))
-        step += 1
-        offset += (n_input+1)
-    #
     while True:
         prompt = "%s words: " % n_input
         sentence = input(prompt)
         sentence = sentence.strip()
         words = sentence.split(' ')
-        if len(words) != n_input:
-            continue
         try:
-            symbols_in_keys = [dictionary[str(words[i])] for i in range(len(words))]
+            symbols_in_keys = [[dictionary[str(words[i])]] for i in range(len(words))]
+            zero_pad = [[0]] * (max_seq - len(words))
+            symbols_in_keys = zero_pad + symbols_in_keys
+            keys = np.reshape(np.array(symbols_in_keys), [-1, max_seq, 1])
             num_next_word = 1
             for i in range(num_next_word):
-                keys = np.reshape(np.array(symbols_in_keys), [-1, n_input, 1])
                 onehot_pred = session.run(pred, feed_dict={x: keys})
                 onehot_pred_index = int(tf.argmax(onehot_pred, 1).eval())
-                sentence = "%s %s" % (sentence,reverse_dictionary[onehot_pred_index])
+                sentence = "%s %s" % (sentence, reverse_dictionary[onehot_pred_index+1])
                 symbols_in_keys = symbols_in_keys[1:]
                 symbols_in_keys.append(onehot_pred_index)
             print(sentence)
         except:
             print("Word not in dictionary")
+
     session.close()
 
 
